fb95c3355abd74f778a671d4dfe32a7a0aedbd22
Kyrielight <jarek13705@gmail.com>
2024-01-16 22:52:48 +0900

    Add validation for Metadata

--- packages/generation/action/__main__.py
+++ packages/generation/action/__main__.py
@@ -4,7 +4,7 @@ from cerberus import Validator
 from enum import Enum
 
 
-# Types of supported Actions
+# Types of supported Actions. Usable as string objects.
 class Action(str, Enum):
     BAN = "BAN"
     KICK = "KICK"
@@ -18,6 +18,15 @@ REQUEST_SCHEMA = {
         'type': 'list',
         'allowed': [Action.TIMEOUT, Action.KICK, Action.BAN, Action.DO_NOTHING],
         'required': False,
+    },
+    'metadata': {
+        'type': 'dict',
+        'required': False,
+        'schema': {
+            'user_id': {'type': 'integer', 'required': False},
+            'guild_id': {'type': 'integer', 'required': False},
+            'channel_id': {'type': 'integer', 'required': False}
+        }
     }
 }
 
@@ -41,23 +50,16 @@ WEIGHTS = [action.get("weight", 0) for action in CONFIG]
 
 
 def main(event):
+    # Validate for correct data.
     validator = Validator(REQUEST_SCHEMA, allow_unknown=True, require_all=True)
-    validated = validator.validate(event)
-
-    if validated:
+    if not validator.validate(event):
         return {
-            "statusCode": 200
+            "body": {"error": validator.errors},
+            "statusCode": 406
         }
 
+    action = random.choices(CONFIG, weights=WEIGHTS, k=1)# # #
     return {
-        "statusCode": 406,
-        "body": {
-            "errorMessage": validator.errors
-        }
+        "body": {"action": action},
+        "statusCode": 200
     }
-
-    # action = random.choices(CONFIG, weights=WEIGHTS, k=1)# # #
-    # return {
-     #   "body": {"action": action},
-     #   "statusCode": 200
-    # }
